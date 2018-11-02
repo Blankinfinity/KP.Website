b3b5a8b207d63a7c2172d9106d6c84ec6c720a5b
Keith Pearce <keith@keith-pearce.co.uk>
2018-11-02 10:07:22 +0000

    Added HttpClientModule and SitedownloadsModule to imports

--- KP.Presentation.UI/src/app/app.module.ts
+++ KP.Presentation.UI/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SitedownloadsModule } from './modules/sitedownloads/sitedownloads.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    SitedownloadsModule,
     AppRoutingModule
   ],
   providers: [],
