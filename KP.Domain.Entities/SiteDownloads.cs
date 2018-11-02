af95725b185b6caf51afa9088635b37f755811dc
Keith Pearce <keith@keith-pearce.co.uk>
2018-11-02 09:54:48 +0000

    Adding model for sitedownloads

---
+++ KP.Domain.Entities/SiteDownloads.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KP.Domain.Entities
+{
+    public class SiteDownloads
+    {
+        public int Id { get; set; }
+        public string SiteName { get; set; }
+        public Nullable<bool> isDownloaded { get; set; }
+        public Nullable<bool> rssSubscribed { get; set; }
+        public Nullable<System.DateTime> dateDownloaded { get; set; }
+        public Nullable<System.DateTime> LastVisited { get; set; }
+    }
+}
