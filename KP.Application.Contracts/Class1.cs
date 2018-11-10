d120aa4404e112db14918761c7abddcc3aec1326
Keith Pearce <keith@keith-pearce.co.uk>
2018-11-10 16:51:04 +0000

    Creating LoggerService with Serilog.

--- KP.Application.Contracts/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace KP.Application.Contracts
-{
-    public class Class1
-    {
-    }
-}
---
+++ KP.Application.Services/LoggerManager.cs
@@ -0,0 +1,34 @@
+﻿using KP.Application.Contracts;
+using Serilog;
+
+namespace KP.Application.Services
+{
+    public class LoggerManager : ILoggerManager
+    {
+        private static ILogger logger = Log.Logger;
+
+        public LoggerManager()
+        {
+        }
+
+        public void LogDebug(string message)
+        {
+            logger.Debug(message);
+        }
+
+        public void LogError(string message)
+        {
+            logger.Error(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            logger.Information(message);
+        }
+
+        public void LogWarn(string message)
+        {
+            logger.Warning(message);
+        }
+    }
+}
