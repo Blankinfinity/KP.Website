690e1850e91173ac04da00f05ac7289d1d297da6
Keith Pearce <keith@keith-pearce.co.uk>
2018-11-10 16:13:21 +0000

    Creating ServiceExtensions static class. Created ConfigureCors method and moved code from startup to here.

---
+++ KP.Presentation.UI/Extensions/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KP.Presentation.UI.Extensions
+{
+  public static class ServiceExtensions
+  {
+    public static void ConfigureCors(this IServiceCollection services)
+    {
+      services.AddCors(options =>
+      {
+        options.AddPolicy("CorsPolicy",
+            builder => builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials());
+      });
+    }
+  }
+}
