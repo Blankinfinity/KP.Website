c64b31ebf5e5eb9b4fca87fced310be15f127bd4
Keith Pearce <keith@keith-pearce.co.uk>
2018-12-16 22:02:24 +0000

    Updating settings, moved to user secrets.

--- KP.Presentation.UI/Startup.cs
+++ KP.Presentation.UI/Startup.cs
@@ -1,8 +1,6 @@
 using System;
 using System.IO;
 using KP.Domain.DapperAttributeMapper;
-using KP.Domain.Entities;
-using KP.Domain.Entities.ExtendedModels;
 using KP.Domain.Entities.Models;
 using KP.Infrastructure.IoC;
 using KP.Infrastructure.Logger;
@@ -20,104 +18,109 @@ using Serilog;
 
 namespace KP.Presentation.UI
 {
-  public class Startup
-  {
-    public IApplicationContainerManager ContainerManager { get; private set; }
-    /// <summary>
-    /// Gets or sets the application configuration, where key value pair settings are stored. See
-    /// http://docs.asp.net/en/latest/fundamentals/configuration.html
-    /// http://weblog.west-wind.com/posts/2015/Jun/03/Strongly-typed-AppSettings-Configuration-in-ASPNET-5
-    /// </summary>
-    public IConfiguration Configuration { get; }
-
-    public Startup(IHostingEnvironment env)
+    public class Startup
     {
-      Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Is(Serilog.Events.LogEventLevel.Debug)
-                .Enrich.FromLogContext()
-                .WriteTo.File(Environment.MachineName + ".log")
-                .CreateLogger();
+        public IApplicationContainerManager ContainerManager { get; private set; }
+        /// <summary>
+        /// Gets or sets the application configuration, where key value pair settings are stored. See
+        /// http://docs.asp.net/en/latest/fundamentals/configuration.html
+        /// http://weblog.west-wind.com/posts/2015/Jun/03/Strongly-typed-AppSettings-Configuration-in-ASPNET-5
+        /// </summary>
+        public IConfiguration Configuration { get; }
 
-      var confBuilder = new ConfigurationBuilder()
-        .SetBasePath(env.ContentRootPath)
-        .AddJsonFile("appsettings.json", true, true)
-        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
-        .AddEnvironmentVariables();
-      Configuration = confBuilder.Build();
-    }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // Changed from void to IServiceProvider for autofac injection of services.
-    public IServiceProvider ConfigureServices(IServiceCollection services)
-    {
-      services.ConfigureCors();
-
-      services.ConfigureIISIntegration();
-
-      services.ConfigureLoggerService();
-
-      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-      // Create the IServiceProvider based on the container.
-      ContainerManager = new AutofacIoCManager();
-      return ContainerManager.PopulateAndGetServiceProvider(services, Configuration);
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, ILogManager logManager)
-    {
-      loggerFactory.AddSerilog(logManager.CreateLogger());
-
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-      else
-      {
-        app.UseHsts();
-      }
-
-      app.UseCors("CorsPolicy");
-
-      app.UseForwardedHeaders(new ForwardedHeadersOptions
-      {
-        ForwardedHeaders = ForwardedHeaders.All
-      });
-
-      //app.UseHttpsRedirection();
-      app.Use(async (context, next) =>
-      {
-        await next();
-
-        //have to ignore api urls here as otherwise we coulgn't get the correct response.
-        //There must be a better way to ignore api calls though
-        if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value) && !context.Request.Path.Value.StartsWith("/api/"))
+        public Startup(IHostingEnvironment env)
         {
-          context.Request.Path = "/index.html"; // Put your Angular root page here 
-          await next();
+            Log.Logger = new LoggerConfiguration()
+                      .MinimumLevel.Is(Serilog.Events.LogEventLevel.Debug)
+                      .Enrich.FromLogContext()
+                      .WriteTo.File(Environment.MachineName + ".log")
+                      .CreateLogger();
+
+            var confBuilder = new ConfigurationBuilder()
+              .SetBasePath(env.ContentRootPath)
+              .AddJsonFile("appsettings.json", true, true)
+              .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+              .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                confBuilder.AddUserSecrets<Startup>();
+            }
+            Configuration = confBuilder.Build();
         }
-      });
 
-      app.UseDefaultFiles();
-
-      // Set to UseStaticFile with Options to stop caching of static content
-      app.UseStaticFiles(new StaticFileOptions
-      {
-        OnPrepareResponse = context =>
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // Changed from void to IServiceProvider for autofac injection of services.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-          var headers = context.Context.Response.GetTypedHeaders();
-          headers.CacheControl = new CacheControlHeaderValue
-          {
-            NoCache = true,
-            NoStore = true
-          };
-        }
-      });
+            services.ConfigureCors();
 
-      app.UseMvcWithDefaultRoute();
-      TypeMapper.Initialize(typeof(Owner));
-      TypeMapper.Initialize(typeof(Account));
-      Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+            services.ConfigureIISIntegration();
+
+            services.ConfigureLoggerService();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Create the IServiceProvider based on the container.
+            ContainerManager = new AutofacIoCManager();
+            return ContainerManager.PopulateAndGetServiceProvider(services, Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, ILogManager logManager)
+        {
+            loggerFactory.AddSerilog(logManager.CreateLogger());
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseCors("CorsPolicy");
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
+
+            //app.UseHttpsRedirection();
+            app.Use(async (context, next) =>
+            {
+                await next();
+
+          //have to ignore api urls here as otherwise we coulgn't get the correct response.
+          //There must be a better way to ignore api calls though
+          if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value) && !context.Request.Path.Value.StartsWith("/api/"))
+                {
+                    context.Request.Path = "/index.html"; // Put your Angular root page here 
+              await next();
+                }
+            });
+
+            app.UseDefaultFiles();
+
+            // Set to UseStaticFile with Options to stop caching of static content
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                OnPrepareResponse = context =>
+                {
+                    var headers = context.Context.Response.GetTypedHeaders();
+                    headers.CacheControl = new CacheControlHeaderValue
+                    {
+                        NoCache = true,
+                        NoStore = true
+                    };
+                }
+            });
+
+            app.UseMvcWithDefaultRoute();
+            TypeMapper.Initialize(typeof(Owner));
+            TypeMapper.Initialize(typeof(Account));
+            Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+        }
     }
-  }
 }
